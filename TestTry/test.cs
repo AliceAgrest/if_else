8a1a5417dcd6fad396dd3ce6e49a464f9f8ceb6c
AliceAgrest <114485470+AliceAgrest@users.noreply.github.com>
2022-10-01 14:22:38 +0300

    Update test.cs

--- TestTry/test.cs
+++ TestTry/test.cs
@@ -1,1 +1,14 @@
 
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class AnotherTest
+    {
+        static void Foo(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Alice");
+        }
+    }
+}
