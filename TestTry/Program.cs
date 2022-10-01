d52ec55f540c8497645e75f47e17d7ae956ee72f
אליסה <Alice@Davids-Mac-mini.local>
2022-10-01 14:13:51 +0300

    Initial commit

---
+++ TestTry/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class TestTry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
