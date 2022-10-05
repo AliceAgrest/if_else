cd1fa28e4955f5ce915b9b4988d1d390f170e6ce
AliceAgrest <alice.agrest@gmail.com>
2022-10-05 14:17:20 +0300

    if statement

--- TestTry/Program.cs
+++ TestTry/Program.cs
@@ -31,22 +31,60 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Console.WriteLine("{0} > {1}", meot, ahadot);
             else
                 Console.WriteLine("Ops the ahadot is bigger then the meot");
-            */
+            
             // answer 3
-            int num, num1, num2 , num3, num4;
+            int num, num1, num2, num3;
             Console.WriteLine("Enter a number three digit: ");
             num = int.Parse(Console.ReadLine());
 
             num1 = num%10;
             num2 = num/100;
             num3 = num % 100 / 10;
-            num4 = num1 + num2;
 
-            if (num4 > num3)
-                Console.WriteLine("The combination of these three numbers can ba a sides of triangle");
+            if ((num1 + num2) > num3 && (num2 + num3) > num1 && (num3 + num1) > num2)
+                Console.WriteLine("The combination of these three numbers can be a sides of triangle");
             else
-                Console.WriteLine("Ops those ");
+                Console.WriteLine("Ops those numbers can't be sides of triangle");
+            
+            // answer 4
+            int kwh, v1 , v2, v3 ;
+            Console.WriteLine("Enter your kWh ");
+            kwh = int.Parse(Console.ReadLine());
 
+            if (kwh == 180)
+            {
+                v1 = (kwh + 70)/ 17 + kwh;
+                Console.WriteLine("Your value is " + v1);
+            }
+            else if (kwh > 180 && kwh < 1000)
+            {
+                v2 = (kwh + 90)/17 + kwh;
+                Console.WriteLine("Your value is " + v2);
+            }
+            else
+            {
+                v3 = kwh / 17 + kwh;
+                Console.WriteLine("our value is " + v3);
+            } 
+            */
+            // answer 5
+            char a, b;
+            Console.WriteLine("Enter a letter ");
+            a = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another letter ");
+            b = char.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("{0} {1}", a, b);
+                Console.WriteLine("{0} {1}", b, a);
+            }
+            else if(b > a)
+            {
+                Console.WriteLine("{0} {1}", b , a);
+                Console.WriteLine("{0} {1}", a , b);
+            }
+            
 
             Console.ReadLine();
         }
